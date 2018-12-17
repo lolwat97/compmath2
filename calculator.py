d5e09519f21469fc065c2b5384b4f980505ff7b9
Sergey Sofeychuk <lolwat97@gmail.com>
2018-12-18 00:52:17 +0300

    ох сука ебал рот

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
+from functools import reduce
+from itertools import combinations
+from operator import add
+from operator import mul
+
 class Calculator:
     def __init__(self):
         self.input = {'x': [0], 'f': [0]}
@@ -16,10 +21,10 @@ class Calculator:
                 self.results['b'].append((elem[0][1], elem[1]))
 
         sorted_coeff = sorted(self.results['b'])
-        der_coeff = [x[1] * x[0] for x in sorted_coeff[1:]]
 
         self.results['b_sort'] = [x[1] for x in sorted_coeff]
-        self.results['b_sort_der'] = der_coeff
+        self.results['coeffs'] = self.newton_to_alg(self.results['b_sort'])
+        self.results['der_coeffs'] = list(map(lambda i: i*self.results['coeffs'][i], range(1, len(self.input['x']))))
 
         for i in range(0, len(self.input['x']) - 1):
             self.results['spline_coeff'].append(self.calculate_spline_coeff(i))
@@ -37,20 +42,39 @@ class Calculator:
         self.results['f'].append(((n_start, n_end), res))
         return res
 
+    def newton_to_alg(self, b_coeff):
+        def numbers_to_xs(tuple):
+            return map(lambda n: self.input['x'][n], tuple)
+
+        def array_of_tuples_to_sum_of_prods(array):
+            arrx = map(numbers_to_xs, array)
+            arr = map(lambda tupl: reduce(mul, tupl), arrx)
+            return reduce(add, arr)
+
+        size = len(self.results['b_sort'])
+
+        coeff = []
+
+        for b in self.results['b_sort']:
+            coeff.append(b)
+
+        for i in range(1, size):
+            for k in range(0, size-i):
+                coeff[k] += (-1)**i * b_coeff[i+k] * array_of_tuples_to_sum_of_prods(list(combinations(range(i+k), i)))
+
+        return coeff
+
     def p(self, x):
-        coeffs = self.results['b_sort']
+        coeffs = self.results['coeffs']
         res = 0
 
         for i in range(len(coeffs)):
-            mult = 1
-            for k in range(i):
-                mult = mult * (x - self.input['x'][k])
-            res += coeffs[i] * mult
+            res += coeffs[i] * x**i
 
         return res
 
     def pder(self, x):
-        coeffs = self.results['b_sort_der']
+        coeffs = self.results['der_coeffs']
         res = 0
 
         for i in range(len(coeffs)):
@@ -76,7 +100,7 @@ class Calculator:
         pderx1 = self.pder(x1)
 
         a3 = (pderx1 * xdiff - 2 * fdiff + pderx * xdiff)/(xdiff**3)
-        a2 = (-1 * pderx1 * xdiff * xsum12 + 3 * fdiff * xsum - pderx * xdiff * xsum12)/(xdiff**3)
+        a2 = (-1 * pderx1 * xdiff * xsum12 + 3 * fdiff * xsum - pderx * xdiff * xsum21)/(xdiff**3)
         a1 = (pderx1 * x * xsum21 * xdiff - 6 * fdiff * x * x1 + pderx * x1 * xsum12 * xdiff)/(xdiff**3)
         a0 = (-1 * pderx1 * x**2 * x1 * xdiff + f1 * x**2 * xdiff31 + f * x1**2 * xdiff13 - pderx * x * x1**2 * xdiff)/(xdiff**3)
 
@@ -84,7 +108,6 @@ class Calculator:
 
     def calculate_spline_at_x(self, x):
         n = self.locate_x(x)
-        print('X is in spline {}'.format(n+1))
         coeffs = self.results['spline_coeff'][n]
         res = coeffs[0] * x**3 + coeffs[1] * x**2 + coeffs[2] * x + coeffs[3]
         return res
--- start.py
+++ start.py
@@ -1,12 +1,13 @@
+x = 1
+
 from calculator import Calculator
 
 calc = Calculator()
 
 calc.set_input(x=[-3, -2, -1, 0, 1, 2, 3],
-               f=[9, 4, 1, 0, 1, 4, 9])
+               f=[10, 5, 2, 1, 2, 5, 10])
 
 calc.calculate()
 calc.print_results()
 
-print(calc.p(2))
-print(calc.calculate_spline_at_x(1))
+print('\n{}'.format(calc.calculate_spline_at_x(x)))
